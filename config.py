b544974f170e67720634f95681d2873c816df4b3
Why You Visit My Profile 😂 <153857610+akdfslftqoetepxvxmzcsibsbm40769374@users.noreply.github.com>
2024-08-14 20:39:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 7400823450))
+OWNER_ID = int(getenv("OWNER_ID", 7374531519)
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PROFESSORxNETWORK")
