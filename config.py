b4358584c699599d73d3382aabcef60ce1125cc8
PROFESSOR <160086180+Fakecheater77x@users.noreply.github.com>
2024-08-20 16:09:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 7374531519)
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PROFESSORxNETWORK")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/WE_NXT")
