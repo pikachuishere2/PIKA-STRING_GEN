2dba1fdefb208d7d1db328b2aa39ffeada96b26d
PROFESSOR <160086180+Fakecheater77x@users.noreply.github.com>
2024-08-20 16:49:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", MONGO_DB_URL=mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
+MONGO_DB_URI = getenv("MONGO_DB_URI", )
 
 OWNER_ID = int(getenv("OWNER_ID", 7374531519)) 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+f37Lk-OMx4tmOGJl")
