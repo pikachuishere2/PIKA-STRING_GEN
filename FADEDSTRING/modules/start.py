f3fc746b4445864dfd973923380f7243c2f7c953
DESTROYER <Rajaryan231207@gmail.com>
2025-10-27 02:57:30 +0530

    Update start.py

--- FADEDSTRING/modules/start.py
+++ FADEDSTRING/modules/start.py
@@ -8,8 +8,9 @@ from FADEDSTRING.utils import add_served_user, keyboard
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ ♡ ✒ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ ➥ ᴛʜɴǫ ғᴏʀ ᴜsɪɴɢ ᴛʜɪs ʙᴏᴛ ✿,ᴍᴀᴅᴇ ʙʏ ➥𝐅ᴀᴅᴇᴅ (@CRY4DED_FR),.",
+        text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ ♡ ✒ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ ➥ ᴛʜɴǫ ғᴏʀ ᴜsɪɴɢ ᴛʜɪs ʙᴏᴛ ✿,ᴍᴀᴅᴇ ʙʏ ➥𝐅ᴀᴅᴇᴅ (@ll_FADED_HU_ll),.",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
     await add_served_user(message.from_user.id)
+
