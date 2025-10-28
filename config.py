b4b3920a8cf8de2ed3ae3c482f28ca5ab138cbd9
pikachuishere2 <escrowertaushif@gmail.com>
2025-10-28 08:49:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", )
+MONGO_DB_URI = getenv("MONGO_DB_URI")
 
 OWNER_ID = int(getenv("OWNER_ID",)) 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/YourSexyGc")
